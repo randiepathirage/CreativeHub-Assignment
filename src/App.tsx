9043c50c78eda67ec64de5f65d6fb01623d25f97
Randie Pathirage <52100121+randiepathirage@users.noreply.github.com>
2023-06-14 19:32:25 +0530

    FEAT: Added the canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from "react";
-import HomePage from "./HomePage";
+import React, { useEffect, useRef } from "react";
+import "./App.css";
 
 const App: React.FC = () => {
-  return <HomePage />;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const context = canvas.getContext("2d");
+      if (context) {
+        context.fillStyle = "white";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+      }
+    }
+  }, []);
+
+  return <canvas ref={canvasRef} className="canvas" />;
 };
 
 export default App;
